df0f57969b8128bd073835ec0d3cdfe17d0eb5bb
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-10-22 22:22:53 -0300

    Added buttons and typography to App.js"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { Add,Settings } from '@mui/icons-material';
 function App() {
   return (
-
     <div>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Contained
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new post
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        It uses h1 styles but it's a p tag
+      </Typography>;
+      <Button variant='contained' sx={{
+        backgroundColor: 'skyblue',
+        color: '#888',
+        margin:5
+      }} >My Unique button</Button>
     </div>
   );
 }
