65a36eb5a305c58080f7586c8dda2a6be3b12634
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-10-25 16:18:37 -0300

    Sidebar.jsx is done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,81 @@
-import { Box } from "@mui/material";
+import { Box,List,ListItem,ListItemButton,ListItemIcon,ListItemText, Switch} from "@mui/material";
+import { Home, Settings ,AccountBox,People, Storefront,Article, Person, ModeNight } from "@mui/icons-material";
 import React from "react";
 
 const Sidebar = () => {
   return (
     <Box
-      bgcolor={"red"}
       flex={1}
       p={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      SideBar
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a"  href="#home" >
+              <ListItemIcon >
+                <Home/>
+              </ListItemIcon> 
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a"  href="#pages" >
+              <ListItemIcon >
+                <Article/>
+              </ListItemIcon> 
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a"  href="#pages" >
+              <ListItemIcon >
+                <People/>
+              </ListItemIcon> 
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a"  href="#pages" >
+              <ListItemIcon >
+                <Storefront/>
+              </ListItemIcon> 
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a"  href="#pages" >
+              <ListItemIcon >
+                <Person/>
+              </ListItemIcon> 
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a"  href="#pages" >
+              <ListItemIcon >
+                <Settings/>
+              </ListItemIcon> 
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a"  href="#pages" >
+              <ListItemIcon >
+                <AccountBox/>
+              </ListItemIcon> 
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a"  href="#pages" >
+              <ListItemIcon >
+                <ModeNight/>
+              </ListItemIcon> 
+              <Switch/>
+            </ListItemButton>
+          </ListItem>
+      </List>
+  
     </Box>
   );
 };
